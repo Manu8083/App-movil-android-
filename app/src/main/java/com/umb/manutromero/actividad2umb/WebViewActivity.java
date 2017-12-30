2ce652559118be7be97111102c6207a4a327fcb3
Manuel Romero <manu8083el@gmail.com>
2017-12-30 15:40:12 -0500

    primer commit

---
+++ app/src/main/java/com/umb/manutromero/actividad2umb/WebViewActivity.java
@@ -0,0 +1,19 @@
+package com.umb.manutromero.actividad2umb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = (WebView) this.findViewById(R.id.webview);
+        webView.loadUrl("http://manuelromero.co/");
+
+    }
+
+}
